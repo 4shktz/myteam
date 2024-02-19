8cd17b50384847e86acbc65ad0a28b079b14840f
hktz <148243209+4shktz@users.noreply.github.com>
2024-02-19 17:14:53 +0100

    added nav hamburger

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const menuBtn = document.querySelector('.hamburger-button');
+const mobileMenu = document.querySelector('nav');
 
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
+})
