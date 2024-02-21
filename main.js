9d305304fa91fb913c3981b74ba84615e5dce215
hktz <148243209+4shktz@users.noreply.github.com>
2024-02-21 09:12:15 +0100

    added the js to contact page

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const menuBtn = document.querySelector('.hamburger-button');
 const mobileMenu = document.querySelector('nav');
 
+const submitBtn = document.querySelector('#submit-button');
+const inputs = document.querySelectorAll('.input');
+const required = document.querySelectorAll('.required');
+
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
+})
+
+submitBtn.addEventListener('click', () => {
+    inputs.forEach((input, el) => {
+        if (input.value === "") {
+            input.classList.add('incorrect');
+            console.log('clicked')
+        } else {
+            input.classList.remove('incorrect');
+        };
+
+        if (input.value === "" && required[el]) {
+            required[el].classList.add('flag');
+        } else {
+            required[el].classList.remove('flag');
+        }
+    })
 })
